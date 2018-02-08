8b3f6675305c93227034380469b41bb9d8f4abb7
gitcjh <1418503647@.qq.com>
2018-02-08 14:07:42 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <my-page title="密码管理">
+        
+        这是首页内容
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
